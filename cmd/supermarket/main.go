4047ae0c7cd2ff76e9750e94c51b26b3d3711b3f
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-02-10 08:20:51 +0300

    cmd: serve http with handlers and services

--- cmd/supermarket/main.go
+++ cmd/supermarket/main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/e2dk4r/supermarket/cockroachdb"
+	myHttp "github.com/e2dk4r/supermarket/http"
+
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
-	fmt.Printf("hello")
+	/* setup service dependencies */
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	/* setup services */
+	productService := &cockroachdb.ProductService{
+		Conn: conn,
+	}
+
+	orderService := &cockroachdb.OrderService{
+		Conn: conn,
+	}
+
+	/* setup http */
+	handler := myHttp.Handler{
+		ProductService: productService,
+		OrderService:   orderService,
+	}
+
+	err = http.ListenAndServe(":8080", &handler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
