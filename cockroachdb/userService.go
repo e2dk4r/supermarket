0c435ef00b39d4cecfc3042030b1a20e008dbeed
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-02-10 08:20:51 +0300

    cockroachdb: userService: fix retryable errors when creating user
    cockroachdb can reject insertion if query was in middle of changing
    related values. (eg. two insert queries at same time)
    we retry if that was the case

--- cockroachdb/userService.go
+++ cockroachdb/userService.go
@@ -3,6 +3,7 @@ package cockroachdb
 import (
 	"context"
 
+	"github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgx"
 	"github.com/e2dk4r/supermarket"
 	"github.com/jackc/pgx/v4"
 )
@@ -16,7 +17,15 @@ func (us *UserService) User(username string) (*supermarket.User, error) {
 	var id string
 	var password string
 
-	row := us.Conn.QueryRow(context.Background(), "SELECT id, password FROM users WHERE username = $1", username)
+	row := us.Conn.QueryRow(context.Background(), `
+	SELECT
+		id,
+		password
+	FROM
+		users
+	WHERE
+		username = $1
+	`, username)
 	err := row.Scan(&id, &password)
 	if err != nil {
 		return nil, err
@@ -36,12 +45,18 @@ func (us *UserService) CreateUser(user *supermarket.User) error {
 	}
 
 	var id string
-	row := us.Conn.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", user.Username, hash)
-	err = row.Scan(&id)
-	if err != nil {
-		return err
-	}
+	crdbpgx.ExecuteTx(context.Background(), us.Conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+		row := tx.QueryRow(context.Background(), `
+		INSERT INTO
+			users (username, password)
+		VALUES
+			($1, $2)
+		RETURNING
+			id
+		`, user.Username, hash)
+		return row.Scan(&id)
+	})
 
 	user.Id = id
-	return nil
+	return err
 }
